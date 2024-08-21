3ab67d2571faef15ee9531b0cfd76452c8cff6ef
Ayush H <jqghebri@gmail.com>
2024-08-22 00:43:14 +0530

    creation

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -3,6 +3,7 @@ import { useSelector, useDispatch } from 'react-redux'
 import { removeTodo } from '../features/todo/todoSlice'
 
 function Todos() {
+    
     const todos = useSelector(state => state.todos)
     const dispatch = useDispatch()
 
